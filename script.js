b8eb82b0e7ac456add1fcad82d719208b88098f6
Shirin183 <rumayarumiya@gmail.com>
2025-04-17 11:34:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector(".box");
+let bgcolor = prompt("enter body backgroundColor");
+
+let color = prompt("enter text color");
+let name = prompt("ismingizi kiriting")
+
+document.body.style.backgroundColor = bgcolor ;
+document.body.style.color = color ;
